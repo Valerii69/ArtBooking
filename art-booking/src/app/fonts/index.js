c311ff901c8100aff1e24cbe335a955547db0de0
Valerii Viun <2455521@gmail.com>
2024-12-05 17:34:25 +0200

    added fonts

---
+++ art-booking/src/app/fonts/index.js
@@ -0,0 +1,13 @@
+import localFont from 'next/font/local';
+
+export const montserratRegular400 = localFont({
+  src: './Montserrat-Regular.ttf',
+});
+
+export const montserratMedium500 = localFont({
+  src: './Montserrat-Medium.ttf',
+});
+
+export const courgetteRegular400 = localFont({
+  src: './Courgette-Regular.ttf',
+});
