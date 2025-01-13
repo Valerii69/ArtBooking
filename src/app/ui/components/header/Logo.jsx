a02ef042a1f14144542d665a3677d1b015079e1d
Valerii Viun <2455521@gmail.com>
2025-01-13 15:51:58 +0200

    Update Logo.jsx

--- src/app/ui/components/header/Logo.jsx
+++ src/app/ui/components/header/Logo.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export function Logo() {
   return (
-    <div className="flex min-h-[35px] flex-row items-center gap-2 text-xl font-bold text-neutral-500">
+    <div className="flex min-h-[35px] flex-row items-center gap-2 text-xl font-bold ">
       <Star />
       <Link href="/" className="my-auto self-stretch">
         Home
